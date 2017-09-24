47994511fec30ef4afedf4e1f0cbfc059b428737
Grace Feng <g.feng@sap.com>
2017-09-24 19:34:26 +0800

    finish the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 
+import { Article } from './article/article.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  articles: Article[];
+  article: Article;
+
+  constructor() {
+    this.articles =[
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular HomePage', 'http://angular.io', 1),
+    ];
+    enum Role {Employee, Manager, Admin};
+    var role: Role = Role.Employee;
+    console.log(role);
+    console.log('Roles: ', Role[0], ',', Role[1], 'and', Role[2]);
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(title);
-    console.log(link);
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    //console.log(title);
+    //console.log(link);
+    //console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit{
 
-  constructor() { }
+  @HostBinding('attr.class') cssClass = 'row';
+
+  @Input() article: Article;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
   }
 
 }
